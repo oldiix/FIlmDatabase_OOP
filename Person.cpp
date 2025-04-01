edf4f2165db1c06079abf6c6ca25841635095b6d
Kateryna Doroftei <dix.collab@gmail.com>
2025-04-01 19:07:21 +0300

    do it a main class with new fields

---
+++ Person.cpp
@@ -0,0 +1,19 @@
+#include "Person.h"
+
+Person::Person() : name("default"), age(0), nationality("ukrainian") {}
+
+Person::Person(const std::string& nm, int a, const std::string& n)
+: name(nm), age(a), nationality(n) {}
+
+Person::Person(const Person& other) : name(other.name), age(other.age), nationality(other.nationality) {}
+
+Person::Person(Person&& other) noexcept
+    : name(std::move(other.name)), age(other.age), nationality(std::move(other.nationality)) {}
+
+
+std::string Person::getName() const { return name; }
+int Person::getAge() const { return age; }
+std::string Person::getNationality() const { return nationality; }
+void Person::setName(const std::string& nm) { name = nm; }
+void Person::setAge(int a) { age = a; }
+void Person::setNationality(const std::string& n) { nationality = n; }
---
+++ Person.h
@@ -0,0 +1,31 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <string>
+
+class Person {
+public:
+    std::string name;
+    int age;
+    std::string nationality;
+
+    Person();
+    Person(const std::string& nm, int a, const std::string& n);
+    Person(const Person& other);
+    Person(Person&& other) noexcept;
+    ~Person() = default;
+
+    Person& operator=(const Person& other) = default;
+    Person& operator=(Person&& other) noexcept = default;
+
+
+    [[nodiscard]] std::string getName() const;
+    [[nodiscard]] int getAge() const;
+    [[nodiscard]] std::string getNationality() const;
+
+    void setName(const std::string& nm);
+    void setAge(int a);
+    void setNationality(const std::string& n);
+
+};
+
+#endif
