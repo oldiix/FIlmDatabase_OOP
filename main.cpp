bfbd00a69bfa9a0b103278b93fa73ff0ccfc6f81
Kateryna Doroftei <dix.collab@gmail.com>
2025-04-01 19:06:27 +0300

    update main to see the actors&reviews

--- main.cpp
+++ main.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
-
 #include "Actor.h"
 #include "Director.h"
 #include "Film.h"
+#include "Review.h"
 
-int main()
-{
-    Film f1("Kill Bill", "Action", 2003);
-    Film f2 = f1;
-    Film f3 = std::move(f1);
+int main() {
+    Director Tarantino("Quentin Tarantino", 62, "American", 27);
 
-    std::cout << "Film 1: " << f1 << std::endl;
-    std::cout << "Film 2/copy: " << f2 << std::endl;
-    std::cout << "Film 3/move: " << f3 << std::endl;
+    Actor Uma("Uma Thurman", 54, "American", 15);
+    Actor Bill("David Carradine", 89, "American", 12);
+    Actor Oren("Lucy Liu", 56, "American", 10);
 
-    std::cout << "Total number of films: " << Film::getNumFilms() << std::endl;
+    Film killBill("Kill Bill", "Action", 2003, Tarantino);
 
-    const Film constFilm("Inception", "Sci-Fi", 2010);
-    std::cout << "Constant Film: " << constFilm.getTitle() << " (" << constFilm.getReleaseYear() << ")" << std::endl;
+    killBill.addActor(Uma);
+    killBill.addActor(Bill);
+    killBill.addActor(Oren);
+
+    // Додавання відгуків
+    killBill.addReview(Review("Masterpiece of action cinema", 10));
+    killBill.addReview(Review("Tarantino is a GOAT", 10));
+    killBill.addReview(Review("Men, that's insanely good movie", 10));
 
 
-    Director d1("Quentin Tarantino", 62, 27);
-    std::cout << "Director: " << d1.getName() << ", Age: " << d1.getAge()
-              << ", Films Directed: " << d1.getFilmsDirected() << std::endl;
+    Film killBillCopy = killBill;
+    Film killBillMoved = std::move(killBill);
 
-    Actor a1("Uma Thurman", 55, 15);
-    std::cout << "Actor: " << a1.getName() << ", Age: " << a1.getAge()
-              << ", Films Acted In: " << a1.getfilmsIn() << std::endl;
+    std::cout << "--- Original Film ---" << std::endl;
+    std::cout << killBill << std::endl;
+
+    std::cout << "\n--- Copied Film ---" << std::endl;
+    std::cout << killBillCopy << std::endl;
+
+    std::cout << "\n--- Moved Film ---" << std::endl;
+    std::cout << killBillMoved << std::endl;
+
+    std::cout << "\nTotal films created: " << Film::getNumFilms() << std::endl;
+
+
+    const Film constFilm("Pulp Fiction", "Crime", 1994, Tarantino);
+    std::cout << "\nConstant Film Info: " << constFilm.getTitle()
+              << " (" << constFilm.getReleaseYear() << ")" << std::endl;
+
+    std::cout << "\nDirector Info: " << Tarantino.getName()
+              << ", Age: " << Tarantino.getAge()
+              << ", Films Directed: " << Tarantino.getFilmsDirected() << std::endl;
+
+
+    std::cout << "\nActor Info: " << Uma.getName()
+              << ", Age: " << Uma.getAge()
+              << ", Films Acted In: " << Uma.getfilmsIn() << std::endl;
 
     return 0;
 }
-
