ce8f8af6cae87bb35e49fea7a9688ae2a62ff74f
Kateryna Doroftei <dix.collab@gmail.com>
2025-04-29 21:45:10 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -1,30 +1,12 @@
-#include <iostream>
-#include "Actor.h"
-#include "Director.h"
-#include "Film.h"
-#include "Review.h"
-#include "FilmDataHandler.h"
-#include "PersonDataHandler.h"
+#include "menu.h"
 
 int main() {
-    Director Tarantino("Quentin Tarantino", 62, "American", 27);
-    Actor Uma("Uma Thurman", 54, "American", 15);
-    Film killBill("Kill Bill", "Action", 2003, Tarantino);
-
-    killBill.addActor(Uma);
-    killBill.addReview(Review("Masterpiece of action cinema", 10));
-
-    std::cout << "=== Film Information ===\n";
-    std::cout << killBill << std::endl;
-
-    // Демонстраційні приклади
-    Person::demonstrateStaticBinding();
-    Actor::demonstrateBasePointer();
-    Director::demonstrateBaseReference();
-    Review::demonstrateFinal();
-    DataHandler::demonstrateInterface();
-
-    std::cout << "\nTotal films created: " << Film::getNumFilms() << std::endl;
-
+    try {
+        menu menu;
+        menu.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Fatal error occurred " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
