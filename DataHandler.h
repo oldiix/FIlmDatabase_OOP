a1b6e51229a23cdeb1026350103fa6df06a8e113
Kateryna Doroftei <dix.collab@gmail.com>
2025-04-29 21:45:45 +0300

    update an interface

--- DataHandler.h
+++ DataHandler.h
@@ -1,18 +1,10 @@
 #ifndef DATAHANDLER_H
 #define DATAHANDLER_H
 
-#include <iostream>
-
 class DataHandler {
 public:
     virtual ~DataHandler() = default;
-    virtual void processData() const = 0;
-    [[nodiscard]] virtual std::string getDatatype() const = 0;
-
-    static void demonstrateInterface()
-    {
-        std::cout << "Demonstrate interface" << std::endl;
-    }
+    virtual void displayInfo() const = 0;
 };
 
 #endif
